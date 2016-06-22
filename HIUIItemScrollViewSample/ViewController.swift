cfcc366ee2d48ecb4f5f23f471914b66c30c0ca8
Sunhong Kim <shkim@sunhong-ui-imac.local>
2016-06-23 01:09:01 +0900

    Initial Commit

---
+++ HIUIItemScrollViewSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HIUIItemScrollViewSample
+//
+//  Created by Sunhong Kim on 2016. 6. 23..
+//  Copyright © 2016년 Sunhong Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
